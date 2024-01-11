7dba7a68d9a2a1e8d4ba78cefbb109215ef47b1d
ohad <ohad.alb@gmail.com>
2024-01-11 21:28:15 +0200

    initial commit

---
+++ app/src/main/java/com/example/partypic2/LoginActivity.kt
@@ -0,0 +1,47 @@
+package com.example.partypic2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+
+class LoginActivity : AppCompatActivity() {
+
+    var emailAddressInput: EditText? = null
+    var passwordInput: EditText? = null
+    var signUpButton: Button? = null
+    var signinButton: Button? = null
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.i("creation","creating signin screen")
+        setContentView(R.layout.login_screen)
+        // Set ui widgets
+        setUI()
+    }
+
+
+    fun setUI(){
+        emailAddressInput = findViewById(R.id.editTextEmailAddress)
+        passwordInput = findViewById(R.id.editTextPassword)
+        signinButton = findViewById(R.id.LoginButton)
+        signUpButton = findViewById(R.id.toSignupPageButton)
+
+        signUpButton?.setOnClickListener{
+            Log.i("buttonClick","signup button in signin screen clicked")
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
+
+        signinButton?.setOnClickListener{
+            Log.i("buttonClick","signup button in signin screen clicked")
+            Log.i("signinSubmit","email input is:" + emailAddressInput?.text.toString())
+            Log.i("signinSubmit","password Input is:" + passwordInput?.text.toString())
+
+//            val intent = Intent(this, SignupActivity::class.java)
+//            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/partypic2/SignupActivity.kt
@@ -0,0 +1,68 @@
+package com.example.partypic2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
+import androidx.activity.result.ActivityResultLauncher
+
+class SignupActivity : AppCompatActivity() {
+
+    // Define all the variables
+    var firstNameInput: EditText? = null
+    var lastNameInput: EditText? = null
+    var emailAddressInput: EditText? = null
+    var passwordInput: EditText? = null
+    var passwordConfirmationInput: EditText? = null
+    var signUpButton: Button? = null
+    var pickProfilepicButton: ImageButton? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.i("creation","creating signup screen")
+        setContentView(R.layout.signup_screen)
+        setUI()
+
+
+    }
+
+    // Define the ui of the screen
+    fun setUI(){
+        firstNameInput = findViewById(R.id.editTextFirstName)
+        lastNameInput = findViewById(R.id.editTextLastName)
+        emailAddressInput = findViewById(R.id.editTextEmailAddress)
+        passwordInput = findViewById(R.id.editTextPassword)
+        passwordConfirmationInput = findViewById(R.id.editTextConfirmPassword)
+        signUpButton = findViewById(R.id.SignupButton)
+        pickProfilepicButton = findViewById(R.id.profilePicButton)
+
+
+        signUpButton?.setOnClickListener{
+            Log.i("buttonClick","signup button in signup screen clicked")
+            Log.i("signupSubmit","first Name Input is:" + firstNameInput?.text.toString())
+            Log.i("signupSubmit","Last Name Input is:" + lastNameInput?.text.toString())
+            Log.i("signupSubmit","email input is:" + emailAddressInput?.text.toString())
+            Log.i("signupSubmit","password Input is:" + passwordInput?.text.toString())
+            Log.i("signupSubmit","password Confirmation Input is:" + passwordConfirmationInput?.text.toString())
+            // check if everything is valid
+            // check if it already exist
+            // create connection to the db for sign up
+        }
+
+
+        pickProfilepicButton?.setOnClickListener{
+            Log.i("buttonClick","pick profile pick button in signup screen clicked")
+
+            //add the logic to take photo or gallery
+
+        }
+
+
+    }
+
+
+}
