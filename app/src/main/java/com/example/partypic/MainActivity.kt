95bce842f6cb092cce653b91ca35b52ba27f90be
ohad <ohad.alb@gmail.com>
2024-03-08 11:05:23 +0200

    basic ui ready

--- app/src/main/java/com/example/partypic/MainActivity.kt
+++ app/src/main/java/com/example/partypic/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.i("creation","creating signin screen")
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login_screen)
 //        setContentView(R.layout.login_screen)
 //        setUI()
 
